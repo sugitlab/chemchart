91cc11986ee1b4e4374e8d05d84ec2a6c190506e
Sugit <sgmt.snj@gmail.com>
2021-10-01 23:48:15 +0900

    feat: plot

---
+++ chemchart/README.md
@@ -0,0 +1,6 @@
+# ChemChart
+
+## Dependencies
+
+- [plotly.js](https://github.com/plotly/plotly.js)
+- [@types/plotly.js](https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/plotly.js)
--- chemchart/src/index.tsx
+++ chemchart/src/index.tsx
@@ -1,11 +1,20 @@
 import React from "react";
+import Plot from "react-plotly.js";
 
 const ChemChart = () => {
   return (
     <div>
-      InputForm
-
-      <input type="text" />
+      hello
+      <Plot
+        data={[
+          {
+            x: [1, 2, 3],
+            y: [1, 2, 3],
+            type: "scatter",
+          },
+        ]}
+        layout={{ width: 320, height: 240, title: "Plot" }}
+      />
     </div>
   );
 };
